73cec920c1d315729a2c7e245d0a01abd815895a
Key <key@gmail.com>
2024-09-26 19:01:05 +0200

    Update 26_09

--- UF1/Activitat_02/RotX.java
+++ UF1/Activitat_02/RotX.java
@@ -5,17 +5,21 @@ public class RotX {
     public static final char[] MIN = "abcçdefghijklmñopqrstuvwxyzàèéíòóúäëïöü".toCharArray();
 
     //Metodo que cifra x posiciones
-    public static String xifraRotX(String cadena, int desplacament){
+    public static String xifraRotX(String msg, int x){
         
     }
 
     //Metodo que descifra x posiciones
-    public static String desxifraRotX(String cadena, int desplacament){
+    public static String desxifraRotX(String msg, int x){
 
     }
 
     //Metodo que probara todas las combinaciones posibles de rotación
-    public static void forcaBrutaRotX(String cadenaXifrada){
+    public static void forcaBrutaRotX(String msg){
+
+    }
+
+    public static void rotaString(String msg){
 
     }
 
