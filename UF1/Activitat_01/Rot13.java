68ea668e65fa9ddf17e4c137e836d064b86138a6
Key <key@gmail.com>
2024-09-26 18:42:33 +0200

    Modificacions de carpetes

--- UF1/Activitat_01/Rot13.java
+++ UF1/Activitat_01/Rot13.java
@@ -1,84 +1,83 @@
 package UF1.Activitat_01;
 
-/*
+import java.util.Scanner;
+
+/* 
  * Programa que es un codificador y descodificador de cifrado César 13 (ROT13).
  */
 
 public class Rot13 {
     // Arrays con las letras del alfabeto
-    public static final char[] MAY = {'A','B','C','Ç','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z','À','È','É','Í','Ò','Ó','Ú'};
-    public static final char[] MIN = {'a','b','c','ç','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z','à','è','é','í','ò','ó','ú'};
+    public static final char[] may = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z','À','È','É','Í','Ò','Ó','Ú'};
+    public static final char[] min = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z','à','è','é','í','ò','ó','ú'};
 
     public static void main(String[] args) {
-        String texto = "Hola :D";
-        String texto1 = "Ñoquis";
-        String texto2 = "ãó";
-
+        Scanner scanner = new Scanner(System.in);
+        String entrada = Scanner.nextLine(scanner)
+        // Ejemplo de prueba
+        String text = "Hola Mundo";
+        
         // Cifrar
-        String cifrado = xifraRot13(pasaAChar(texto));
-        String cifrado1 = xifraRot13(pasaAChar(texto1));
-        String cifrado2 = xifraRot13(pasaAChar(texto2));
-        System.out.println("Texto cifrado: " + cifrado + "\n" + cifrado1 + "\n" + cifrado2);
-
+        String cifrado = charToString(xifraRot13(pasaAChar(text)));
+        System.out.println("Texto cifrado: " + cifrado);
+        
         // Descifrar
-        String descifrado = desxifraRot13(pasaAChar(cifrado));
-        String descifrado1 = desxifraRot13(pasaAChar(cifrado1));
-        String descifrado2 = desxifraRot13(pasaAChar(cifrado2));
-        System.out.println("Texto descifrado: " + descifrado + "\n" + descifrado1 + "\n" + descifrado2);
+        String descifrado = charToString(desxifraRot13(pasaAChar(cifrado)));
+        System.out.println("Texto descifrado: " + descifrado);
     }
 
     // Función para cifrar usando ROT13
-    public static String xifraRot13(char[] lista) {
+    public static char[] xifraRot13(char[] lista) {
         // Recorre la lista de caracteres y cifra
         for (int i = 0; i < lista.length; i++) {
             char letraActual = lista[i];
 
-            // Comprobar si es MAYúscula
-            for (int y = 0; y < MAY.length; y++) {
-                if (letraActual == MAY[y]) {
-                    lista[i] = MAY[(y + 13) % MAY.length];
-                    break;
+            // Comprobar si es mayúscula
+            for (int y = 0; y < may.length; y++) {
+                if (letraActual == may[y]) {
+                    // Aplica la rotación de 13 posiciones para mayúsculas
+                    lista[i] = may[(y + 13) % may.length];
+                    break; // No es necesario seguir buscando después de encontrar la letra
                 }
             }
 
-            // Comprobar si es MINúscula
-            for (int y = 0; y < MIN.length; y++) {
-                if (letraActual == MIN[y]) {
-                    lista[i] = MIN[(y + 13) % MIN.length];
+            // Comprobar si es minúscula
+            for (int y = 0; y < min.length; y++) {
+                if (letraActual == min[y]) {
+                    // Aplica la rotación de 13 posiciones para minúsculas
+                    lista[i] = min[(y + 13) % min.length];
                     break;
                 }
             }
         }
-
-        return charToString(lista);
+        return lista;
     }
 
     // Función para descifrar usando ROT13
-    public static String desxifraRot13(char[] lista) {
+    public static char[] desxifraRot13(char[] lista) {
         // Recorre la lista de caracteres y descifra
         for (int i = 0; i < lista.length; i++) {
             char letraActual = lista[i];
 
-            // Comprobar si es MAYúscula
-            for (int y = 0; y < MAY.length; y++) {
-                if (letraActual == MAY[y]) {
-                    int nuevaPos = (y - 13 + MAY.length) % MAY.length;
-                    lista[i] = MAY[nuevaPos];
+            // Comprobar si es mayúscula
+            for (int y = 0; y < may.length; y++) {
+                if (letraActual == may[y]) {
+                    // Aplica la rotación inversa de 13 posiciones para mayúsculas
+                    lista[i] = may[(y + 13) % may.length];
                     break;
                 }
             }
 
-            // Comprobar si es MINúscula
-            for (int y = 0; y < MIN.length; y++) {
-                if (letraActual == MIN[y]) {
-                    int nuevaPos = (y - 13 + MIN.length) % MIN.length;
-                    lista[i] = MIN[nuevaPos];
+            // Comprobar si es minúscula
+            for (int y = 0; y < min.length; y++) {
+                if (letraActual == min[y]) {
+                    // Aplica la rotación inversa de 13 posiciones para minúsculas
+                    lista[i] = min[(y + 13) % min.length];
                     break;
                 }
             }
         }
-
-        return charToString(lista);
+        return lista;
     }
 
     /* Pasar un String a un array de char */
---
+++ UF1/Activitat_02/RotX.java
@@ -0,0 +1,21 @@
+/* Version mas compleja y con complementos del cifrado de Cesar */
+
+public class RotX {
+    public static final char[] MAY = "ABCÇDEFGHIJKLMÑOPQRSTUVWXYZÀÈÉÍÒÓÚÄËÏÖÜ".toCharArray();
+    public static final char[] MIN = "abcçdefghijklmñopqrstuvwxyzàèéíòóúäëïöü".toCharArray();
+
+    //Metodo que cifra x posiciones
+    public static String xifraRotX(String cadena, int desplacament){
+        
+    }
+
+    //Metodo que descifra x posiciones
+    public static String desxifraRotX(String cadena, int desplacament){
+
+    }
+
+    //Metodo que probara todas las combinaciones posibles de rotación
+    public static void forcaBrutaRotX(String cadenaXifrada){
+
+    }
+}
