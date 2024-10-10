3347627bf04039b3e325d51b7b25ba2cd114da0f
Key <key@gmail.com>
2024-10-10 17:02:17 +0200

    Se ha solucionado un error

--- UF1/03-Monoalfa/Monoalfabetic.java
+++ UF1/03-Monoalfa/Monoalfabetic.java
@@ -1,68 +1,72 @@
 /* Programa que cifrara y desfirara teniendo como referencia un alfabeto aleatorio generado por un modulo */
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Monoalfabetic {
     public static final char[] MAY = "ABCÇDEFGHIJKLMÑOPQRSTUVWXYZÀÈÉÍÒÓÚÄËÏÖÜ".toCharArray();
-    public static final char[] RANDOM = new char[MAY.length];
+    public static final char[] MIN = "abcçdefghijklmñopqrstuvwxyzàèéíòóúäëïöü".toCharArray();
 
-    // Metode que generar la permutacio del alfabet
-    public static char[] permutaAlfabet(char[] alfabet, char[] alfaRandom){
-        ArrayList<Character> lista = anadirLetras(alfabet);
-        Collections.shuffle(lista);
-        for(int i=0; i < alfaRandom.length; i++){
-            char letra = lista.get(i);
-            alfaRandom[i] = letra;
-        }
-        return alfaRandom;
+    // Método que cifra x posiciones
+    public static String xifraRotX(String msg, int x) {
+        return rotaString(msg, true, x);
     }
 
-    //Metode que xifrara
-    public static String xifraMonoAlfa(String cadena){
-        StringBuilder resultado = new StringBuilder();
-        for(int i=0; i < cadena.length(); i++){
-            char letraAct = cadena.charAt(i);
-            if(Character.isLowerCase(letraAct)){
-                letraAct = Character.toUpperCase(letraAct);
-                letraAct = posicion(letraAct);
-                letraAct = Character.toLowerCase(letraAct);
-                resultado.append(letraAct);
-            }else{
-                letraAct = posicion(letraAct);
-                resultado.append(letraAct);
-            }
+    // Método que descifra x posiciones
+    public static String desxifraRotX(String msg, int x) {
+        return rotaString(msg, false, x);
+    }
+
+    // Método que prueba todas las combinaciones posibles de rotación
+    public static void forcaBrutaRotX(String msg) {
+        for (int i = 1; i <= 15; i++) { // Corregido para probar desde rotación 1 hasta 15
+            System.out.println("Rotación +" + i + ": " + rotaString(msg, true, i));
         }
+    }
+
+    // Método genérico para cifrar o descifrar
+    public static String rotaString(String msg, boolean cifrar, int num) {
+        StringBuilder resultado = new StringBuilder();
+
+        for (int i = 0; i < msg.length(); i++) {
+            char letra = msg.charAt(i);
+            if (Character.isUpperCase(letra)) {
+                letra = rotaChar(letra, MAY, cifrar, num);
+            } else if (Character.isLowerCase(letra)) {
+                letra = rotaChar(letra, MIN, cifrar, num);
+            }
+            resultado.append(letra); // Añadir directamente la letra (rotada o no)
+        }
+
         return resultado.toString();
     }
 
-    //Metode que desxifrara
-    //public static String desxifraMonoAlfa(String cadena){}
-
-    //Metodo que añade las letras del alfabeto MAY a un ArrayList<Character>
-    public static ArrayList<Character> anadirLetras(char[] alfabeto){
-        ArrayList<Character> list = new ArrayList<Character>();
-        for(int i=0; i < alfabeto.length; i++){
-            list.add(alfabeto[i]);
-        }
-        return list;
-    }
-
-    //Es para saber que letra ira en el cifrado
-    // Busca la posicion en el alfabeto MAY y despues busca la misma posicion el el alfabeto Random
-    public static char posicion(char letra){
-        int posicion = 0;
-        for(int i=0; i < MAY.length; i++){
-            if(letra == MAY[i]){
-                posicion = i;
+    // Método auxiliar para rotar una letra dentro del array de MAY o MIN
+    public static char rotaChar(char letra, char[] alfabeto, boolean cifrar, int num) {
+        for (int y = 0; y < alfabeto.length; y++) {
+            if (letra == alfabeto[y]) {
+                int nuevaPos;
+                if (cifrar) {
+                    nuevaPos = (y + num) % alfabeto.length;
+                } else {
+                    nuevaPos = (y - num + alfabeto.length) % alfabeto.length;
+                }
+                return alfabeto[nuevaPos];
             }
         }
-        return RANDOM[posicion];
+        return letra; // Si no es una letra del alfabeto, devolver la letra original
     }
 
     public static void main(String[] args) {
-        String prueba1 = "Hola me llamo Keyla :3";
-        System.out.println("Texto normal: " + prueba1);
-        System.out.println("Texto cifrado" + xifraMonoAlfa(prueba1));
+        String text = "Hola me llamo Keyla :D";
+
+        // Cifrar con rotación 5
+        String cifrado = xifraRotX(text, 5);
+        System.out.println("Texto cifrado: " + cifrado);
+
+        // Descifrar con rotación 5
+        String descifrado = desxifraRotX(cifrado, 5);
+        System.out.println("Texto descifrado: " + descifrado);
+
+        // Fuerza bruta para probar todas las rotaciones
+        System.out.println("\nPrueba de fuerza bruta:");
+        forcaBrutaRotX(text);
     }
 }
